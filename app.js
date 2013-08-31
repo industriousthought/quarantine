4af652211a0dca70ab3b2c098d512effdf28fd82
Brian Blackwell <industriousthought@gmail.com>
2013-08-31 10:43:32 -0400

    updated socket.io configuration

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 server.listen(80);
 
 app.get('/joystick.png', function (req, res) {
