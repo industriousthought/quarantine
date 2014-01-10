ec2ba87a16f5998cd5b2ef8d1cf83fcd1eac7581
Brian Blackwell <industriousthought@gmail.com>
2014-01-10 00:21:09 -0800

    Improved game flow, postgreSQL. And Stuff.

--- app.js
+++ app.js
@@ -1,13 +1,177 @@
+var pg = require('pg');
 var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
-//io.configure(function () { 
-  //io.set("transports", ["xhr-polling"]); 
-  //io.set("polling duration", 10); 
-//});
+server.listen(80);
 
-server.listen(process.env.PORT);
+var conString = "postgres://user:test@localhost:5432/postgres";
+
+var client = new pg.Client(conString);
+client.connect();
+
+//client.query('CREATE TABLE users(id VARCHAR(32), nick VARCHAR(32), fbID VARCHAR(32));');
+//client.query('INSERT INTO users(id, nick, fbID) VALUES(\'1\', \'brian\', \'Doe\');');
+//client.query('INSERT INTO users(id, nick) VALUES("id", "a");');
+
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/splash.html');
+});
+
+var games = [];
+var lobbies = [];
+var players = [];
+var pause = false;
+var pauseUser = false;
+
+var makeGame = function(p) {
+
+    var game = {};
+    game.id = p.id;
+    game.players = [];
+    game.lastPlayer = 0;
+    
+    lobbies[game.id] = io.of('/lobby' + game.id).on('connection', function (socket) {
+
+        for (var p in game.players) {
+            lobbies[game.id].emit('newPlayer', {
+                'id': p
+            });
+
+        }
+        socket.on('cancleLobby', function (data) {
+            delete lobbies[game.id];
+            delete games[game.id];
+
+        });
+    });
+
+    game.controller = io.of('/con' + game.id).on('connection', function (socket) {
+
+        socket.on('event', function (data) {
+            
+            game.display.emit('event', {
+                'x': data.x,
+                'y': data.y,
+                'controller': data.controller,
+                'id': data.id
+
+          });
+        });
+
+        socket.on('newPlayer', function (data) {
+            if (!players[data.id]) {
+                client.query('INSERT INTO users(id, nick, fbID) VALUES(\'' + data.id + '\', \'' + data.nick + '\', \'Doe\');');
+                players[data.id] = {};
+                players[data.id].nick = data.nick;
+            }
+
+            lobbies[game.id].emit('newPlayer', {
+                'id': data.id,
+                'nick': players[data.id].nick 
+            });
+            game.players[data.id] = {'nick': players[data.id].nick};
+
+        });
+
+        socket.on('checkPlayer', function(data) {
+            if (players[data.id]) {
+                game.controller.emit('idExists', {
+                    id: data.id,
+                    nick: players[data.id].nick
+                });
+            } else {
+                game.controller.emit('idNotExist', {
+                    true: 'true'
+                });
+          
+            }
+        });
+
+        socket.on('pause', function(data) {
+            if (!pauseUser) { 
+                pauseUser = data.id;
+            } else {
+                game.display.emit('pause', {
+                    true: true
+                });
+            console.log('pause sent');
+            }
+            console.log('pause pressed');
+
+        });
+
+    });
+            
+    game.display = io.of('/dis' + game.id).on('connection', function (socket) {
+
+        console.log('display connected!');
+
+        for (var p in game.players) {
+            game.display.emit('event', {
+                'newPlayer': p
+            });
+        }
+
+        socket.on('deadPlayer', function (data) {
+            game.players[data.id].living = null;
+            game.controller.emit('deadPlayer', {
+                'id': data.id
+
+        });
+
+
+    });
+});
+ 
+
+}
+
+app.get('/newGame', function(req, res) {
+    var gameID = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        gameID += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    res.send(gameID);
+
+
+    games[gameID] = makeGame({id: gameID});
+});
+
+app.get('/img/cancle.gif', function(req, res) {
+  res.sendfile(__dirname + '/img/cancle.gif');
+});
+
+app.get('/img/gas_mask.gif', function(req, res) {
+  res.sendfile(__dirname + '/img/gas_mask.gif');
+});
+
+app.get('/css/splashStyle.css', function(req, res) {
+  res.sendfile(__dirname + '/css/splashStyle.css');
+});
+
+app.get('/js/animationEffects.js', function(req, res) {
+  res.sendfile(__dirname + '/js/animationEffects.js');
+});
+
+app.get('/js/mathTools.js', function(req, res) {
+  res.sendfile(__dirname + '/js/mathTools.js');
+});
+
+app.get('/img/help.gif', function(req, res) {
+  res.sendfile(__dirname + '/img/help.gif');
+});
+
+app.get('/img/newGame.gif', function(req, res) {
+  res.sendfile(__dirname + '/img/newGame.gif');
+});
+
+app.get('/img/settings.gif', function(req, res) {
+  res.sendfile(__dirname + '/img/settings.gif');
+});
 
 app.get('/joystick.png', function (req, res) {
   res.sendfile(__dirname + '/joystick.png');
@@ -77,123 +241,16 @@ app.get('/display', function (req, res) {
   res.sendfile(__dirname + '/display.html');
 });
 
+app.get('/fb.html', function (req, res) {
+  res.sendfile(__dirname + '/fb.html');
+});
+
 io.sockets.on('connection', function (socket) {
   socket.emit('event', { hello: 'world' });
   socket.on('my other event', function (data) {
-    console.log(data);
   });
 });
 
-var lastPlayer = 0;
-var players = [];
-var display = null;
-
-var con = io.of('/con').on('connection', function (socket) {
-    socket.on('event', function (data) {
-        
-        dis.emit('event', {
-            'x': data.x,
-            'y': data.y,
-            'controller': data.controller,
-            'id': data.id
-
-      });
-    });
-
-    socket.on('pingBack', function(data) {
-        if (players[data.id]) {
-            players[data.id].outstandingPing = null;
-        }
-
-    });
-    
-    socket.on('newPlayer', function (data) {
-        var newPlayer = {};
-        
-        if (data.id && players[data.id]) {
-            newPlayer = players[data.id];
-            players[data.id].alive = true;
-        } else {
-            newPlayer.id = data.id;
-            newPlayer.nick = data.nick;
-            newPlayer.alive = true;
-            players[newPlayer.id] = newPlayer;
-        }
-        
-        console.log(data.id + ', ' + newPlayer.id);
-
-        dis.emit('event', {
-            'newPlayer': newPlayer.id,
-            'nick': newPlayer.nick
-        });
-
-        lastPlayer++;
-
-    });
 
 
-    console.log('new controller');
-  });
 
-var dis= io.of('/dis').on('connection', function (socket) {
-
-    display = {};
-
-    socket.on('deadPlayer', function (data) {
-        players[data.id].living = null;
-        con.emit('deadPlayer', {
-            'id': data.id
-        });
-
-    });
-
-    socket.on('pingBack', function(data) {
-        display.outstandingPing = null;
-
-    });
-    
-});
- 
-var pingSendInterval = setInterval(function() {
-    var pingId = new Date().getTime();
-    var i = 0;
-    for (i in players) {
-        players[i].outstandingPing = pingId;
-    }
-    if (display) {
-        display.outstandingPing = pingId;
-    }
-
-    dis.emit('ping', {
-        time: pingId
-    });
-
-    con.emit('ping', {
-        time: pingId
-    });
-
-}, 1000);
-
-var pingCheckInterval = setInterval(function() {
-    var now = new Date().getTime();
-    var elapsedTime = 0;
-    for (i in players) {
-        if (players[i].outstandingPing) {
-            elapsedTime = now - players[i].outstandingPing;
-            if (elapsedTime > 500) {
-                console.log('player ID: ' + i + ' is experienceing latency of ' + elapsedTime + '.');
-            }
-            players[i].outstandingPing = null;
-        }
-    }
-
-    if (display && display.outstandingPing) {
-        elapsedTime = now - display.outstandingPing;
-        if (elapsedTime > 500) {
-            console.log('Display is experienceing latency of ' + elapsedTime + '.');
-            display.outstandingPing = null;
-        }
-    }
-
-            
-}, 50);
